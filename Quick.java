f9af3daf222c7f14a0d79fca6130e96235f96ac4
Ali Taoube <ataoube00@stuy.edu>
2019-03-13 17:33:29 -0400

    Improved partition.

--- Quick.java
+++ Quick.java
@@ -3,8 +3,11 @@ import java.util.Random;
 public class Quick{
   public static void main(String[] args) {
     int[] data = {999,999,999,4,1,0,3,2,999,999,999};
-    partition(data, 0, data.length - 1);
-    toString(data);
+
+    System.out.print(partition(data, 0, data.length - 1));
+    System.out.println();
+    // quicksort(data);
+    System.out.print(toString(data));
   }
 
   private static void swap(int[] arr, int x, int y) {
@@ -17,40 +20,54 @@ public class Quick{
 
 	    int pivot = (int) (Math.random() * (data.length - 1));
       int pivotData = data[pivot];
-      // int pivot = new Random().nextInt(end - start + 1);
-
-      System.out.print("PIVOT : " + pivot);
-      System.out.println();
-      System.out.print("PIVOTDATA : " + pivotData);
-      System.out.println();
 
 	    swap(data, start, pivot);
-	    start++;
 
 	    while (end != start){
-	      if (data[start] >= pivotData){ // If it's greater, put it at the end
-	        swap(data, start, end);
+	      if (data[start + 1] >= pivotData){ // If it's greater, put it at the end
+	        swap(data, start + 1, end);
 	        end--;
 	      }
 	      else{
+          swap(data, start, start + 1);
 	        start++;
 	      }
 	    }
-
-	    if (data[start] < pivotData){
-	      swap(data, start, 0); // At the end, if num at 0 is less than the pivot at 0, switch
-        System.out.print(toString(data));
-        return start;
-	    }
-	    else{
-	      swap(data, start - 1, 0); // If data[start ] > pivot, that means the num right before it is less than pivot
-        System.out.print(toString(data));
-	      return start - 1;
-	    }
+      return start;
 	  }
 
-  public static int quickselect(int[] data, int k){
-    return -1;
+  // public static int quickselect(int[] data, int k){
+  //   partition(data, 0, 0);
+  //
+  //   for (int x = 0; x < data.l)
+  // }
+
+  public static void quicksort(int[] data){
+    quickSortH(data, 0, data.length - 1);
+  }
+
+  private static void quickSortH(int[] data, int start, int end){
+    if (start >= end) return;
+    // System.out.print("START1: " + start);
+    // System.out.println();
+    int pivot = partition(data, start, end);
+
+    System.out.println("DATA");
+    System.out.println("PIVOT: " + pivot);
+    System.out.print(toString(data));
+    System.out.println();
+    // System.out.println();
+    // System.out.print("START: " + start);
+    // System.out.println();
+    // System.out.print("END: " + end);
+    // System.out.println();
+    // System.out.print("LENGTH: " + data.length);
+    // System.out.println();
+    // System.out.print("PIVOT: " + pivot);
+    // System.out.println();
+
+    quickSortH(data, start, pivot);
+    quickSortH(data, pivot + 1, end);
   }
 
   public static String toString(int[] data){
