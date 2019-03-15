ba7e76723c38da86be82e50c7e2ca448832a0818
Ali Taoube <ataoube00@stuy.edu>
2019-03-14 23:09:56 -0400

    quicksort works but gets very slow on repeating arrays

--- Quick.java
+++ Quick.java
@@ -2,55 +2,6 @@ import java.util.Random;
 import java.util.Arrays;
 
 public class Quick{
-  // public static void main(String[] args) {
-    // int[] data = {999,999,999,4,1,0,3,2,999,999,999};
-    //
-    // // System.out.print(partition(data, 0, data.length - 1));
-    // // System.out.println();
-    // quicksort(data);
-    // // System.out.print(toString(data));
-    // System.out.print(toString(data));
-    public static void main(String[]args){
-      int[] data = {999,999,999,4,1,0,3,2,999,999,999};
-      quicksort(data);
-      System.out.print(toString(data));
-    //
-    // System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-    // int[]MAX_LIST = {1000000000,500,10};
-    // for(int MAX : MAX_LIST){
-    //   for(int size = 31250; size < 2000001; size*=2){
-    //     long qtime=0;
-    //     long btime=0;
-    //     //average of 5 sorts.
-    //     for(int trial = 0 ; trial <=5; trial++){
-    //       int []data1 = new int[size];
-    //       int []data2 = new int[size];
-    //       for(int i = 0; i < data1.length; i++){
-    //         data1[i] = (int)(Math.random()*MAX);
-    //         data2[i] = data1[i];
-    //       }
-    //       long t1,t2;
-    //       t1 = System.currentTimeMillis();
-    //       Quick.quicksort(data2);
-    //       t2 = System.currentTimeMillis();
-    //       qtime += t2 - t1;
-    //       t1 = System.currentTimeMillis();
-    //       Arrays.sort(data1);
-    //       t2 = System.currentTimeMillis();
-    //       btime+= t2 - t1;
-    //       if(!Arrays.equals(data1,data2)){
-    //         System.out.println("FAIL TO SORT!");
-    //         System.exit(0);
-    //       }
-    //     }
-    //     System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-    //   }
-    //   System.out.println();
-    // }
-  }
-
-  // }
-
   private static void swap(int[] arr, int x, int y) {
     int temp = arr[x];
     arr[x] = arr[y];
@@ -58,17 +9,10 @@ public class Quick{
   }
 
   private static int getMedian(int[] data, int start, int end){
-    // System.out.println("StART:" + start);
-    // System.out.println("end: " + end);
-
     int first = data[start];
     int middle = data[start + (end - start) / 2];
     int last = data[end];
     int median = start;
-
-    // System.out.print("FIRST: " + first);
-    // System.out.print("MIDDLE: " + middle);
-    // System.out.print("LAST: " + last);
 
     if ((first <= last && first >= middle) || (first >= last && first <= middle)) median = start;
     if ((last <= first && last >= middle) || (last >= first && last <= middle)) median = end;
@@ -87,7 +31,11 @@ public class Quick{
     pivot = start;
 
     while (end > start) {
-      if ((data[start] > pivotData) || (Math.random() >= .50 && data[start] == pivotData)) { // if value is greater than pivot value, move to the end
+      if ((data[start] > pivotData)) { // if value is greater than pivot value, move to the end
+        System.out.println();
+        System.out.println("START: " + start);
+        System.out.println("END: " + end);
+        System.out.println("PIVOT: " + pivot);
         System.out.println(toString(data));
           swap(data, start, end);
           end--;
@@ -138,6 +86,4 @@ public class Quick{
     }
     return output;
   }
-
-
 }
